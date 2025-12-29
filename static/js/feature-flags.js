ff740fe8b26b6733888f7f7e01bd5ef398430412
jesus-justin <mjesusjustin@gmail.com>
2025-12-29 16:26:43 +0800

    Add documentation header to feature-flags.js

--- static/js/feature-flags.js
+++ static/js/feature-flags.js
@@ -1,3 +1,8 @@
+/**
+ * feature-flags.js
+ * Enhanced with better documentation
+ * Last updated: 2025-12-29
+ */
 <!-- HTML Enhancement: Feature Flagging Support -->
 <script>
     window.FeatureFlags = {
@@ -31,3 +36,4 @@
         });
     });
 </script>
+
